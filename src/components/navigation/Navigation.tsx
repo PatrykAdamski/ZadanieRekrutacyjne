a65df4bca6f02841aa1ea187da9b977e95eea9b1
PatrykAdamski <patada7090@gmail.com>
2022-05-17 12:04:27 +0200

    Created responsive navigation

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { ButtonHamburger } from '../buttonHamburger/ButtonHamburger';
+
+export const Navigation = () => {
+  const [isMenuActive, setMenuActive] = useState(false);
+
+  return (
+    <nav
+      className={`nav
+    ${isMenuActive ? 'nav--active' : ''}`}
+    >
+      <p className="nav__logo">
+        <span>E</span>-book
+      </p>
+      <ButtonHamburger
+        isMenuActive={isMenuActive}
+        setMenuActive={setMenuActive}
+      />
+      <ul className="nav__list">
+        <a href="/#" className="nav__item">
+          Strona główna
+        </a>
+        <a href="/#" className="nav__item">
+          Ulubione
+        </a>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/views/Root.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Navigation } from '../components/navigation/Navigation';
+
+export const Root = () => {
+  return <Navigation />;
+};
